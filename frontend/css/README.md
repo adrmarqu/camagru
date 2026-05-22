4fd06eacd72ec748d3f9ab24b563f22d06d9918e
adrmarqu <adria.marquez.3b@gmail.com>
2026-05-22 20:25:38 +0200

    css

---
+++ frontend/css/README.md
@@ -0,0 +1,68 @@
+# BASE (no clases)
+root (init 0 + colors)
+body
+h1
+h2
+h3
+h4
+p
+
+# COMPONENTS
+## BUTTON
+btn
+btn-drop                       
+btn-primary      
+btn-secondary                
+btn-danger              
+## LINK
+link ---------> (link normal)
+link.header
+link.footer
+## IMG
+icon
+image
+btn-image
+
+# FORM
+form
+form-input
+form-checkbox
+form-group
+form-group.reverse
+
+# HEADER
+#home
+#nav-auth
+#nav-guest
+nav-guest-pc
+nav-guest-mobile
+## DROPDOWN BURGUER
+burguer
+burguer span
+burguer.active span:nth-child(1)
+burguer.active span:nth-child(2)
+burguer.active span:nth-child(3)
+## DROPDOWN
+dropdown
+dropdown-list
+dropdown:hover dropdown-list
+dropdown:hover dropdown-btn
+
+Galeria*
+Editor*
+Mis fotos
+Mis favoritos
+Settings
+
+Login*
+Signin*
+
+* :Responive
+
+# FOOTER
+
+# WEBCAM
+
+# GALLERY
+
+# SETTINGS
--- frontend/scripts/header.js
+++ frontend/scripts/header.js
@@ -2,6 +2,10 @@ const btn = document.getElementById("change");
 const n1 = document.getElementById("nav-auth");
 const n2 = document.getElementById("nav-guest");
 const logout = document.getElementById("logout");
+const burguer = document.getElementById("burguer-guest");
+const dropGuest = document.getElementById("drop-guest");
+const dropAuth = document.getElementById("drop-auth");
+const btnDrop = document.getElementById("btn-drop");
 
 const change = () =>
 {
@@ -9,9 +13,34 @@ const change = () =>
     n2.classList.toggle("hidden");
 };
 
-btn.addEventListener("click", () => change());
-logout.addEventListener("click", () =>
+const change2 = () =>
 {
     n1.classList.add("hidden");
     n2.classList.remove("hidden");
-});
+};
+
+const burguerActivate = () =>
+{
+    burguer.classList.toggle("active");
+    dropGuest.classList.toggle("hidden");
+};
+
+const dropActivate = () =>
+{
+    dropAuth.classList.toggle("hidden");
+}; 
+
+const hide = (e) =>
+{
+    if (e.target == btnDrop || e.target == burguer) return ;
+
+    dropAuth.classList.add("hidden");
+    burguer.classList.remove("active");
+    dropGuest.classList.add("hidden");
+}
+
+btn.addEventListener("click", () => change());
+logout.addEventListener("click", () => change2());
+burguer.addEventListener("click", () => burguerActivate());
+btnDrop.addEventListener("click", () => dropActivate());
+document.addEventListener("click", (e) => hide(e));
